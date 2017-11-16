74ccc2a35773ac6f8039705a016a30f07216fe27
Alex Ellis <alexellis2@gmail.com>
2017-11-16 12:04:31 +0000

    ASCII
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ asciilogo/handler.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const figlet = require('figlet');
+
+module.exports = (context, callback) => {
+    figlet(context, (err, val) => {
+        if(err) {
+            return callback(undefined, err.toString());    
+        }
+        callback(undefined, val);
+    });
+}
